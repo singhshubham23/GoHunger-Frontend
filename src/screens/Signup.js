494bedcae89cc42706a99d9a3fe85c2bf18c8467
Shubham Kumar <142138865+singhshubham23@users.noreply.github.com>
2025-11-16 15:28:22 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";  
+
 export default function Signup() {
-const navigate = useNavigate();
+
+   const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
