482bed99cc84132c335f28e1b2da5556a9aa0820
Shubham Kumar <142138865+singhshubham23@users.noreply.github.com>
2025-11-16 14:59:56 +0530

    Add useNavigate to Signup component

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";  
+import { Link, useNavigate } from "react-router-dom";
 const navigate = useNavigate();
 export default function Signup() {
   const [formData, setFormData] = useState({
